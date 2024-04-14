53c234d3709a658c877310edcc615b09730f3c6a
Blues24 <lukmanaffandi900@gmail.com>
2024-04-14 22:03:59 +0700

    updated file

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-window.alert(`hurry up pick the port`).setTimeout(10000);
-document.getElementById("text").textContent = "Welcome to the dock";
+document.getElementById("text").textContent = "Welcome to the Terminal";
+
+var inactivitTime = function(){
+    var time;
+    window.onload = resetTimer;
+    //DOM event
+    document.onmousemove = resetTimer;
+    document.onkeydown = resetTimer;
+
+
+
+function dock(){
+    console.log("The user has been afk for too long");
+    console.error("Its time to wakeup the user");
+    window.alert("Wake up Sleepy head")
+    window.alert("pick the port")
+    
+}
+
+function resetTimer(){
+    clearTimeout(time);
+    time = setTimeout(dock, 60000)
+}
+};
+
+inactivitTime();
