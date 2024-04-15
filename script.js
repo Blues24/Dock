7964ada5f0a2bf5ef5e3a401e317c36c48b67aeb
Blues24 <lukmanaffandi900@gmail.com>
2024-04-15 15:10:33 +0700

    update the files

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-document.getElementById("text").textContent = "Welcome to the Terminal";
 
+document.getElementById("text").textContent = "Welcome to the Terminal";
+//detect the user idle time
 var inactivitTime = function(){
     var time;
     window.onload = resetTimer;
     //DOM event
     document.onmousemove = resetTimer;
     document.onkeydown = resetTimer;
-
-
-
+//to tell the user to wake up
 function dock(){
     console.log("The user has been afk for too long");
     console.error("Its time to wakeup the user");
@@ -16,11 +15,11 @@ function dock(){
     window.alert("pick the port")
     
 }
-
+//timer function
 function resetTimer(){
     clearTimeout(time);
     time = setTimeout(dock, 60000)
 }
 };
-
+//call the function
 inactivitTime();
